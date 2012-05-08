afffe6e8110ca1bf256778c215bf94416a0e7f62
Nate Wiger <nate@wiger.org>
2012-05-08 13:49:25 -0700

    asm

--- voidptr/main.cpp
+++ voidptr/main.cpp
@@ -18,23 +18,27 @@ using namespace std;
  
      // Load a sprite to display
      sf::Texture texture;
-     if (!texture.loadFromFile("cute_image.jpg"))
+     if (!texture.loadFromFile("nmm_coaster_gray.jpg"))
          return EXIT_FAILURE;
      sf::Sprite sprite(texture);
  
      // Create a graphical text to display
      sf::Font font;
-     if (!font.loadFromFile("arial.ttf"))
+     if (!font.loadFromFile("/Library/Fonts/Arial.ttf"))
          return EXIT_FAILURE;
      sf::Text text("Hello SFML", font, 50);
  
      // Load a music to play
-     sf::Music music;
-     if (!music.openFromFile("nice_music.ogg"))
-         return EXIT_FAILURE;
+//     sf::Music music;
+//     if (!music.openFromFile("nice_music.ogg"))
+//         return EXIT_FAILURE;
 
      // Play the music
-     music.play();
+     //music.play();
+
+     char str[80];
+     int i=0;
+     float sx=0.f, sy=0.f;  // sprite position
  
      // Start the game loop
      while (window.isOpen())
@@ -44,16 +48,69 @@ using namespace std;
          while (window.pollEvent(event))
          {
              // Close window : exit
-             if (event.type == sf::Event::Closed)
-                 window.close();
+             switch(event.type)
+             {
+                 case sf::Event::Closed:
+                     window.close();
+                     break;
+             }
          }
- 
+
+         // Move the sprite around
+         // One offset == one pixel, but moves so fast, we need
+         // to use a smaller value
+         float x=0.f, y=0.f;
+         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ||
+             sf::Keyboard::isKeyPressed(sf::Keyboard::A) )
+             x -= 0.5; // move left
+         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ||
+             sf::Keyboard::isKeyPressed(sf::Keyboard::D) )
+             x += 0.5; // move right
+         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
+             sf::Keyboard::isKeyPressed(sf::Keyboard::W) )
+             y -= 0.5; // move up
+         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ||
+             sf::Keyboard::isKeyPressed(sf::Keyboard::S) )
+             y += 0.5; // move down
+
+         bool animate_guy = false;
+         if (x != 0 || y != 0)
+             animate_guy = true;
+         
          // Clear screen
          window.clear();
  
+         sprite.move(x, y);
+
+         // animation
+         if (animate_guy)
+         {
+            sprintf(str, "animate(%d)=%f,%f\n", i, x, y);
+            sf::Text animtext(str, font, 20);
+            sf::Vector2f vec(0.f, 100.f);
+            animtext.move(vec);
+            window.draw(animtext);
+         }
+ 
          // Draw the sprite
          window.draw(sprite);
- 
+
+         sx, sy = sprite.getPosition().x, sprite.getPosition().y;
+
+            sprintf(str, "sprite=%f,%f\n", sx, sy);
+            sf::Text sptxt(str, font, 20);
+            sptxt.move(300.f, 0.f);
+            window.draw(sptxt);
+
+
+         // counter
+         sprintf(str, "i=%d\n", i++);
+         sf::Text countext(str, font, 10);
+         sf::Vector2f vec(100.f, 100.f);
+         countext.move(vec);
+         window.draw(countext);
+
+         
          // Draw the string
          window.draw(text);
  
